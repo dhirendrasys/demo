1610c0bb1792d5224e65b500187cebeac0065789
dhirendrasys <110660408+dhirendrasys@users.noreply.github.com>
2022-10-06 09:20:50 +0530

    Update dip_driver_route.sql

--- dip_driver_route.sql
+++ dip_driver_route.sql
@@ -1,12 +1,7 @@
-CREATE TABLE IF NOT EXISTS stg.cicd_demo
-(
-        column1 VARCHAR(225)
-        ,column2 VARCHAR(225)
-)
-DISTSTYLE KEY
-DISTKEY (column1)
-SORTKEY (
-        column1
-        )
-;
-ALTER TABLE stg.cicd_demo owner to svc_informatica;
+ CREATE TABLE CICD_demo(
+   ID INT PRIMARY KEY     NOT NULL,
+   NAME           TEXT    NOT NULL,
+   AGE            INT     NOT NULL,
+   ADDRESS        CHAR(50),
+   SALARY         REAL
+);
