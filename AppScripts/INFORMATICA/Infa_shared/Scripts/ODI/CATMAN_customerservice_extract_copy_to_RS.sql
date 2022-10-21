2cdd5f160ea79b4ff1fdbb4023cee500faa842e2
dhirendrasys <110660408+dhirendrasys@users.noreply.github.com>
2022-10-21 14:36:59 +0530

    Update CATMAN_customerservice_extract_copy_to_RS.sql

--- AppScripts/INFORMATICA/Infa_shared/Scripts/ODI/CATMAN_customerservice_extract_copy_to_RS.sql
+++ AppScripts/INFORMATICA/Infa_shared/Scripts/ODI/CATMAN_customerservice_extract_copy_to_RS.sql
@@ -1,7 +1,7 @@
 Truncate table stg.catman_customerservicelevelextract;
 
 COPY stg.catman_customerservicelevelextract
-FROM '{S3_DEV_BUCKET_NAME}/CATMAN_Weekly_Extracts/s_Extract_Catman_CustomerService'
+FROM 'S3_DEV_BUCKET_NAME/CATMAN_Weekly_Extracts/s_Extract_Catman_CustomerService'
 IAM_ROLE '{DEV_IAM_ROLE}'
 DELIMITER '|'
 ACCEPTINVCHARS AS '?'
