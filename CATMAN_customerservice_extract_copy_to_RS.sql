3ea6ffcb74e13c8ab64cdd181ecdd876d3857ce9
dhirendrasys <110660408+dhirendrasys@users.noreply.github.com>
2022-10-21 13:59:50 +0530

    Update CATMAN_customerservice_extract_copy_to_RS.sql

--- CATMAN_customerservice_extract_copy_to_RS.sql
+++ CATMAN_customerservice_extract_copy_to_RS.sql
@@ -1,8 +1,8 @@
 Truncate table stg.catman_customerservicelevelextract;
 
 COPY stg.catman_customerservicelevelextract
-FROM '${S3_DEV_BUCKET_NAME}/CATMAN_Weekly_Extracts/s_Extract_Catman_CustomerService'
-IAM_ROLE '${DEV_IAM_ROLE}'
+FROM '{S3_DEV_BUCKET_NAME}/CATMAN_Weekly_Extracts/s_Extract_Catman_CustomerService'
+IAM_ROLE '{DEV_IAM_ROLE}'
 DELIMITER '|'
 ACCEPTINVCHARS AS '?'
 dateformat 'auto'
