285f8929676b256a192a4dbccc92f9d8037af868
dhirendrasys <110660408+dhirendrasys@users.noreply.github.com>
2022-10-21 15:36:19 +0530

    Update ADP_daily_adp_warehousetimecardextract_copy_to_RS.sql

--- AppScripts/INFORMATICA/Infa_shared/Scripts/ODI/ADP_daily_adp_warehousetimecardextract_copy_to_RS.sql
+++ AppScripts/INFORMATICA/Infa_shared/Scripts/ODI/ADP_daily_adp_warehousetimecardextract_copy_to_RS.sql
@@ -2,7 +2,7 @@ truncate table stg.adp_warehousetimecardextract;COPY stg.adp_warehousetimecardex
 
 
 
-FROM '${S3_DEV_BUCKET_NAME}/ADPWhsepaycode/PBPWHSE_'
+FROM 'S3_DEV_BUCKET_NAME/ADPWhsepaycode/PBPWHSE_'
 IAM_ROLE '${DEV_IAM_ROLE}''
 DELIMITER ','
 ACCEPTINVCHARS AS '?'
