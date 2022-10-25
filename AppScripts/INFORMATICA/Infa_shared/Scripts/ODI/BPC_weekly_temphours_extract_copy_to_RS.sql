ac313fa847dbb456ec35ac4f9f1b677b539e2510
dhirendrasys <110660408+dhirendrasys@users.noreply.github.com>
2022-10-25 11:27:15 +0530

    Update BPC_weekly_temphours_extract_copy_to_RS.sql

--- AppScripts/INFORMATICA/Infa_shared/Scripts/ODI/BPC_weekly_temphours_extract_copy_to_RS.sql
+++ AppScripts/INFORMATICA/Infa_shared/Scripts/ODI/BPC_weekly_temphours_extract_copy_to_RS.sql
@@ -1,8 +1,8 @@
 truncate table stg.bpc_temphoursextract;
 
 COPY stg.bpc_temphoursextract
-FROM '${S3_DEV_BUCKET_NAME}/BPC/BPCTempHoursExtract.txt.gz'
-IAM_ROLE '${DEV_IAM_ROLE}'
+FROM 'S3_DEV_BUCKET_NAME/BPC/BPCTempHoursExtract.txt.gz'
+IAM_ROLE 'DEV_IAM_ROLE'
 DELIMITER '\t'
 ACCEPTINVCHARS AS '?'
 dateformat 'auto'
