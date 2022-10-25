a2355ed45dd958d93aaffc1dfa7b82dde70a951a
dhirendrasys <110660408+dhirendrasys@users.noreply.github.com>
2022-10-25 11:26:21 +0530

    Update AsianFoods_weekly_pieces_extract_copy_to_RS.sql

--- AppScripts/INFORMATICA/Infa_shared/Scripts/ODI/AsianFoods_weekly_pieces_extract_copy_to_RS.sql
+++ AppScripts/INFORMATICA/Infa_shared/Scripts/ODI/AsianFoods_weekly_pieces_extract_copy_to_RS.sql
@@ -1,8 +1,8 @@
 truncate table stg.stage_asianfoodspiecesextract;
 
 COPY stg.stage_asianfoodspiecesextract
-FROM '${S3_DEV_BUCKET_NAME}/Other/AsianFoodsPieces/Asian'
-IAM_ROLE '${DEV_IAM_ROLE}'
+FROM 'S3_DEV_BUCKET_NAME/Other/AsianFoodsPieces/Asian'
+IAM_ROLE 'DEV_IAM_ROLE'
 DELIMITER ','
 ACCEPTINVCHARS AS '?'
 dateformat 'auto'
