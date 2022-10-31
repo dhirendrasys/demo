3069279440cf0e83ec14c82910c7247d9b0a8915
dhirendrasys <dhiren.aws@gmail.com>
2022-10-31 15:08:15 +0530

    folder structure

---
+++ AppScripts/INFORMATICA/Infa_shared/Scripts/ODI/Scripts/BPCHC_weekly_hc_extract_copy_to_RS.sh
@@ -0,0 +1,13 @@
+truncate table stg.bpc_temphcextract;
+
+COPY stg.bpc_temphcextract
+FROM 'S3_DEV_BUCKET_NAME/BPCHC/BPCTempHCExtract.txt.gz'
+IAM_ROLE 'DEV_IAM_ROLE'
+DELIMITER '\t'
+ACCEPTINVCHARS AS '?'
+dateformat 'auto'
+timeformat 'auto'
+IGNOREHEADER AS 1
+REMOVEQUOTES
+GZIP
+;
